12692de0274dea6b28cbacda6ad2bdfe6aeb11fe
devcxvcx <155403521+devcxvcx@users.noreply.github.com>
2024-01-19 17:07:11 +0900

    1

---
+++ .history/src/app/layout_20240118101022.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from 'next'
+import '@styles/globals.css'
+import {CookiesProvider} from 'react-cookie';
+import RecoilRootProvider from './_utils/recoilRootProvider'
+import Header from '@components/Header'
+
+export const metadata: Metadata = {
+    title: 'My Apps',
+    description: '루트',
+    icons: {
+        icon: "/favicon.png",
+    },
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <RecoilRootProvider>
+    <html lang="en">
+        <body>
+            <Header />
+            <section>
+                {children}
+            </section>
+            <footer>
+                footer
+            </footer>
+        </body>
+    </html>
+    </RecoilRootProvider>
+  )
+}
